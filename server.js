7fe1739e5ad30b04fefd7dfd27a3caa9cfeaf145
sengathit <sengathit.l@gmail.com>
2018-10-10 10:30:39 -0500

    added cors res.header

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 
-let corsOptions = {
-    origin: '*',
-    methods: ['GET','PUT','POST','DELETE']
-}
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 const sketchSchema = require('./models/sketch.model');
 const dbURL = 'mongodb://sengathit:Slavanh77@ds117423.mlab.com:17423/sketch';
