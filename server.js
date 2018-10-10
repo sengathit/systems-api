7fe5cf15ae82d3cd8557f5f4095a40a455a511a8
sengathit <sengathit.l@gmail.com>
2018-10-10 00:37:27 -0500

    uploading file images

--- server.js
+++ server.js
@@ -4,31 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 
-let corsOptions = {
-    origin: '*'
-}
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors());
 
 const sketchSchema = require('./models/sketch.model');
 const dbURL = 'mongodb://sengathit:Slavanh77@ds117423.mlab.com:17423/sketch';
@@ -48,7 +26,7 @@ app.get('/api/photos',(req,res) => {
     },err => res.status(400).send(err));
 });
 
-app.post('/api/photos',(req,res) => {
+app.put('/api/photos',(req,res) => {
     let body = req.body;
     let upload = new doodles({title: body.title, description: body.description, img: body.img});
     upload.save((err,doc) => {
