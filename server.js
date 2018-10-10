37b6c812bf47f6b64273e541ba765d753f3139e3
sengathit <sengathit.l@gmail.com>
2018-10-10 10:25:08 -0500

    adding cors options

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 
+let corsOptions = {
+    origin: '*',
+    methods: ['GET','PUT','POST','DELETE']
+}
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const sketchSchema = require('./models/sketch.model');
 const dbURL = 'mongodb://sengathit:Slavanh77@ds117423.mlab.com:17423/sketch';
