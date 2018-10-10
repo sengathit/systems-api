abfb3aa4658c06d7b70cf86cf09d67f96b0ac2f1
sengathit <sengathit.l@gmail.com>
2018-10-09 20:56:21 -0500

    fixing mongoose save()

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const dbURL = 'mongodb://sengathit:Slavanh77@ds117423.mlab.com:17423/sketch';
 mongoose.connect(dbURL,{ useNewUrlParser: true });
 
 const port = process.env.PORT || 3000;
-let doodles = mongoose.model('Doodles', sketchSchema,'doodles');
+let doodles = mongoose.model('Doodles', sketchSchema);
 
 app.get('/',(req, res) => {
     res.send('Hello world');
@@ -29,11 +29,11 @@ app.get('/api/photos',(req,res) => {
 app.post('/api/photos',(req,res) => {
     let body = req.body;
     let upload = new doodles({title: body.title,description: body.description, img: body.img});
-    upload.save((err,doc) => {
+    upload.save((err) => {
         if(err) {
             res.status(400).send(err);
         }else{
-            res.send(doc);
+            res.send('saved');
         }
     })
     
